6620ff64c87858ed17c3273cef616973e32c5f89
domGFX <domGFX@mail.com>
2021-06-14 16:11:14 +0200

    fin du layout

--- public/assets/js/menu.js
+++ public/assets/js/menu.js
@@ -4,14 +4,30 @@ const overlay = document.querySelector(".overlay");
 
 hamburger.addEventListener("click", mobileMenu);
 
-function mobileMenu() {
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function mobileMenu() {
     if (overlay.style.visibility == 'visible') {
         overlay.style.visibility = 'hidden';
+ //       await sleep(300);
+ //       navMenu.style.transition = 'none';
     } else {
+        navMenu.style.transition = '0.3s';
         overlay.style.visibility = 'visible';
     }
 
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+
 }
 
+/*
+$(window).bind('scroll', function () {
+    if ($(window).scrollTop() > 0) {
+        $('.app-header').addClass('active');
+    } else {
+        $('.app-header').removeClass('active');
+    }
+})*/
