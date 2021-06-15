e54d4e394b8de0c9b4354ed2dfa7295e7aeadbfe
domGFX <domGFX@mail.com>
2021-06-16 01:08:50 +0200

    homepage edit template (responsive à finir)

--- public/assets/js/menu.js
+++ public/assets/js/menu.js
@@ -4,30 +4,18 @@ const overlay = document.querySelector(".overlay");
 
 hamburger.addEventListener("click", mobileMenu);
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
 async function mobileMenu() {
-    if (overlay.style.visibility == 'visible') {
-        overlay.style.visibility = 'hidden';
- //       await sleep(300);
- //       navMenu.style.transition = 'none';
-    } else {
-        navMenu.style.transition = '0.3s';
-        overlay.style.visibility = 'visible';
+    var isInEditMode = overlay.classList.contains('editOverlay');
+
+    if (isInEditMode == false) {
+        if (overlay.style.visibility == 'visible') {
+            overlay.style.visibility = 'hidden';
+        } else {
+            navMenu.style.transition = '0.3s';
+            overlay.style.visibility = 'visible';
+        }
     }
 
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
-
 }
-
-/*
-$(window).bind('scroll', function () {
-    if ($(window).scrollTop() > 0) {
-        $('.app-header').addClass('active');
-    } else {
-        $('.app-header').removeClass('active');
-    }
-})*/
--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -13,8 +13,7 @@ class HomepageController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('homepage/index.html.twig', [
-            'controller_name' => 'HomepageController',
-        ]);
+ return $this->render('homepage/edit.html.twig');
+    //        return $this->render('homepage/index.html.twig');
     }
 }
