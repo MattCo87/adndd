27bc1e9c762d02dcf2ed75c81185a92012125d1f
Nicolas Vauché <hello@nicolasvauche.net>
2021-06-18 15:38:04 +0200

    End

--- migrations/Version20210618133706.php
+++ migrations/Version20210618133706.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210618112518 extends AbstractMigration
+final class Version20210618133706 extends AbstractMigration
 {
     public function getDescription(): string
     {
--- src/Controller/GamesController.php
+++ src/Controller/GamesController.php
@@ -15,4 +15,20 @@ class GamesController extends AbstractController
     {
         return $this->render('games/index.html.twig');
     }
+
+    /**
+     * @Route("/ajouter-un-jeu", name="games.add")
+     */
+    public function add(): Response
+    {
+        return $this->render('games/add.html.twig');
+    }
+
+    /**
+     * @Route("/ajouter-un-jeu-de-des", name="games.adddiceset")
+     */
+    public function addDiceset(): Response
+    {
+        return $this->render('games/adddiceset.html.twig');
+    }
 }
