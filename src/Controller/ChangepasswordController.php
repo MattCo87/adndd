1588ce5fee25f00e8dffeadcac56cc3237c0d28f
matt co <87700a@gmail>
2021-06-22 04:23:14 +0200

    Template change password OK

--- src/Controller/ChangepasswordController.php
+++ src/Controller/ChangepasswordController.php
@@ -6,69 +6,47 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Forms;
-use App\Form\ChangepasswordType;
+use App\Form\AccountType;
 use Symfony\Component\Form\Form;
 use Symfony\Component\HttpFoundation\Request;
-
-
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Form\FormError;
+use App\Form\ChangepasswordType;
 use App\Entity\User;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 
 class ChangepasswordController extends AbstractController
 {
     /**
-     * @Route("/changepassword", name="changepassword"
-     */
+     * @Route("/changepassword", name="changepassword")
+    */
 
-    public function index(Request $request): Response
+    public function index(Request $request, UserPasswordEncoderInterface $encoder): Response
     {
+        $entityManager = $this->getDoctrine()->getManager();
         $user = $this->getUser();
 
         $form = $this->createForm(ChangepasswordType::class, $user);
 
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-/*            $entityManager = $this->getDoctrine()->getManager();
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $plainPassword = $request->request->get('changepassword')['plainPassword']['second'];
+            $encoded = $encoder->encodePassword($user, $plainPassword);
+
+            $user->setPassword($encoded);
 
             $entityManager->persist($user);
             $entityManager->flush();
 
             $this->addFlash('notice', 'Votre mot de passe à bien été changé !');
 
-            return $this->redirectToRoute('account');
+            return $this->redirectToRoute('app_logout');
         }
 
         return $this->render('account/changepassword.html.twig', [
             'formChangepassword' => $form->createView()
         ]);
-*/      
-
-
-            $passwordEncoder = $this->get('security.password_encoder');
-            $oldPassword = $request->request->get('')['oldPassword'];
-            // Si l'ancien mot de passe est bon
-            if ($passwordEncoder->isPasswordValid($user, $oldPassword))
-            {
-                $newEncodedPassword = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
-var_dump($request->request);die();
-                $user->setPassword($newEncodedPassword);
-
-                $em->persist($user);
-                $em->flush();
-
-                $this->addFlash('notice', 'Votre mot de passe à bien été changé !');
-
-                return $this->redirectToRoute('account');
-            } else {
-                $form->addError(new FormError('Ancien mot de passe incorrect'));
-            }
-
-
-
-        }
     }
 }
--- src/Form/ChangepasswordType.php
+++ src/Form/ChangepasswordType.php
@@ -9,63 +9,30 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class ChangepasswordType extends AbstractType
 {
-        
-
-    /**
-     * @Assert\NotBlank()
-     * @Assert\Length(max=4096)
-     */
-    private $plainPassword;
-
-    /**
-     * The below length depends on the "algorithm" you use for encoding
-     * the password, but this works well with bcrypt.
-     *
-     * @ORM\Column(type="string", length=64)
-     */
-    private $password;
-
-    public function getPlainPassword()
-    {
-        return $this->plainPassword;
-    }
-
-    public function setPlainPassword($password)
-    {
-        $this->plainPassword = $password;
-    }
-
-    public function getPassword()
-    {
-        return $this->password;
-    }
-
-    public function setPassword($password)
-    {
-        $this->password = $password;
-    }
-
-    public function getSalt()
-    {
-        // The bcrypt and argon2i algorithms don't require a separate salt.
-        // You *may* need a real salt if you choose a different encoder.
-        return null;
-    }
-
-
-
-
-
-
-
-
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-/*        $builder
-            ->add('password')
+        $user = new User();
+                    
+        $builder
+            ->add('password', PasswordType::class, array(
+                'mapped' => false,
+                'label' => 'Ancien mot de passe: ',
+            ))
+
+            ->add('plainPassword', RepeatedType::class, array(
+                'type' => PasswordType::class,
+                'mapped' => false,
+                'invalid_message' => 'Les deux mots de passe doivent être identiques',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options'  => ['label' => 'Nouveau mot de passe: '],
+                'second_options' => ['label' => 'Confirmer mot de passe: '],
+            ))
+
             ->add('save', SubmitType::class,
             [
                 'label' => 'Envoyez !',
@@ -74,26 +41,6 @@ class ChangepasswordType extends AbstractType
                         'class' => 'form-sumbit',
                     ],
             ])
-        ; */
-        $user = new User();
-                    
-        $builder
-            ->add('oldPassword', PasswordType::class, array(
-                'mapped' => false
-            ))
-
-            ->add('plainPassword', RepeatedType::class, array(
-                'type' => PasswordType::class,
-                'invalid_message' => 'Les deux mots de passe doivent être identiques',
-                'options' => array(
-                    'attr' => array(
-                        'class' => 'password-field'
-                    )
-                ),
-                'required' => true,
-            ))
-
-            ->add('submit', SubmitType::class)
         ;
     }
 
